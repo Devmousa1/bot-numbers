b0529ff232ad22bab5ec1e8d04b134a425bad2b1
Mohammed Foud <mfoud444@gmail.com>
2024-10-30 21:56:11 +0300

    first commit pp

--- web/t.php
+++ web/t.php
@@ -1,5 +1,7 @@
 <?php
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ob_start();
 // Fetch the JSON data from the URL and decode it into an associative array
 $data = json_decode(file_get_contents("https://5sim.biz/v1/guest/prices?country=vietnam&product=telegram"), true);
 $adds = $data["vietnam"]["telegram"]["virtual6"]['count'];
@@ -9,7 +11,7 @@ $price = $data["vietnam"]["telegram"]['virtual16']['cost'];
 // Check if data is successfully fetched and decoded
 if ($data !== null) {
     // Print the data for debugging
-    print($price);
+    print_r($data);
 } else {
     echo "Failed to retrieve or decode JSON data.";
 }
