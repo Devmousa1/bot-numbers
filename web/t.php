73808bd17d763d63f92e022467795fbcc0d7486a
Mohammed Foud <mfoud444@gmail.com>
2024-10-31 00:38:56 +0300

    first commit pp

--- web/t.php
+++ web/t.php
@@ -3,10 +3,10 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ob_start();
 // Fetch the JSON data from the URL and decode it into an associative array
-$data = json_decode(file_get_contents("https://5sim.biz/v1/guest/prices?country=vietnam&product=telegram"), true);
+$data = json_decode(file_get_contents("https://5sim.biz/v1/guest/prices?country=poland&product=telegram"), true);
 
-$price = $data['vietnam']['telegram'];
-$add = $data['vietnam']['106']['count'];
+$price = $data['poland']['telegram']['orange']['cost'];
+// $add = $data['vietnam']['106']['count'];
 
 // Check if data is successfully fetched and decoded
 if ($data !== null) {
